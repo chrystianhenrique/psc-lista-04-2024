4bb4bde4b1113b5c1c595cda539d6eeb93682f04
Chrystian Henrique <161754767+chrystianhenrique@users.noreply.github.com>
2024-03-23 12:59:04 -0300

    Add files via upload
    Faça um programa que imprima na tela os números de 1 a 20, um abaixo do
    outro. Depois modifique o programa para que ele mostre os números um ao
    lado do outro.

---
+++ Exercicio 6 (Lado do outro).java
@@ -0,0 +1,8 @@
+public class Main {
+    public static void main(String[] args) {
+        // Imprime os números de 1 a 20 um abaixo do outro
+        for (int i = 1; i <= 20; i++) {
+            System.out.println(i);
+        }
+    }
+}
